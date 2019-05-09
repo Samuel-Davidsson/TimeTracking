222be7c02896e499b10761d2e17291240956b5c8
Samuel <sam.davidsson86@outlook.com>
2019-05-09 22:13:56 +0200

    Refactor: Refactored the Loginform

--- timetracking/src/Routes/LoginRoute/LoginForm.js
+++ timetracking/src/Routes/LoginRoute/LoginForm.js
@@ -1,56 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import "./Login.css";
 
-class LoginForm extends React.Component {
-  loginRef = React.createRef();
-  passwordRef = React.createRef();
+const loginForm = props => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
     const userLoginInfo = {
-      login: this.loginRef.current.value,
-      password: this.passwordRef.current.value
+      login: login,
+      password: password
     };
-    this.props.tryLogin(userLoginInfo);
+    props.tryLogin(userLoginInfo);
   };
-  render() {
-    return (
-      <div className="login-form-div">
-        <Form className="login-form" onSubmit={this.handleSubmit}>
-          <FormGroup row>
-            <Label className="login-form-label">Användarnamn</Label>
-            <Input
-              name="username"
-              innerRef={this.loginRef}
-              placeholder="Användarnamn.."
-              type="email"
-              required
-              className="username-username"
-            />
-          </FormGroup>
-          <FormGroup row>
-            <Label className="login-form-label">Lösenord</Label>
-            <Input
-              name="password"
-              innerRef={this.passwordRef}
-              placeholder="Lösenord.."
-              type="password"
-              required
-              className="username-password"
-            />
-          </FormGroup>
-          <p className="login-comment">
-            Lätt sätt att fylla i sin tidsrapport.
-          </p>
-          <div className="login-button-div">
-            <Button color="info" type="submit">
-              Logga in
-            </Button>
-          </div>
-        </Form>
-      </div>
-    );
-  }
-}
-export default LoginForm;
+  return (
+    <div className="login-form-div">
+      <Form className="login-form" onSubmit={handleSubmit}>
+        <FormGroup row>
+          <Label className="login-form-label">Användarnamn</Label>
+          <Input
+            name="username"
+            value={login}
+            onChange={e => setLogin(e.target.value)}
+            placeholder="Användarnamn.."
+            type="email"
+            required
+            className="username-username"
+          />
+        </FormGroup>
+        <FormGroup row>
+          <Label className="login-form-label">Lösenord</Label>
+          <Input
+            name="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            placeholder="Lösenord.."
+            type="password"
+            required
+            className="username-password"
+          />
+        </FormGroup>
+        <p className="login-comment">Lätt sätt att fylla i sin tidsrapport.</p>
+        <div className="login-button-div">
+          <Button color="info" type="submit">
+            Logga in
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+export default loginForm;
