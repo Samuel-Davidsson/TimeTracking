f743e4076a51dbec13f310da1d7088fdf7cffbbb
Samuel <sam.davidsson86@outlook.com>
2019-05-09 22:18:03 +0200

    Refactor: Refactored adminForm to func comp

--- timetracking/src/Routes/AdminRoute/AdminForm.js
+++ timetracking/src/Routes/AdminRoute/AdminForm.js
@@ -1,40 +1,39 @@
 import React from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 
-export default class AdminForm extends React.Component {
-  render() {
-    return (
-      <div className="adminform-form">
-        <Form onSubmit={this.props.handleCheckboxesSubmit}>
-          <FormGroup check>
-            <Label />
-            <Input
-              type="checkbox"
-              checked={this.props.attest}
-              onChange={this.props.handleAttestCheckBoxStatus}
-            />{" "}
-            Attestera
-          </FormGroup>
-          <FormGroup check>
-            <Label />
-            <Input
-              type="checkbox"
-              checked={this.props.accepted}
-              onChange={this.props.handleAcceptedCheckBoxStatus}
-            />{" "}
-            Låsa upp
-          </FormGroup>
-          <div className="adminform-button">
-            <Button
-              color="info"
-              type="submit"
-              onClick={this.props.handleCheckBoxClicked}
-            >
-              Spara
-            </Button>
-          </div>
-        </Form>
-      </div>
-    );
-  }
-}
+const adminForm = props => {
+  return (
+    <div className="adminform-form" hidden={props.reportId === 0}>
+      <Form onSubmit={props.handleCheckboxesSubmit}>
+        <FormGroup check>
+          <Label />
+          <Input
+            type="checkbox"
+            checked={props.attest}
+            onChange={props.handleAttestCheckBoxStatus}
+          />{" "}
+          Attestera
+        </FormGroup>
+        <FormGroup check>
+          <Label />
+          <Input
+            type="checkbox"
+            checked={props.accepted}
+            onChange={props.handleAcceptedCheckBoxStatus}
+          />{" "}
+          Låsa upp
+        </FormGroup>
+        <div className="adminform-button">
+          <Button
+            color="info"
+            type="submit"
+            onClick={props.handleCheckBoxClicked}
+          >
+            Spara
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+export default adminForm;
