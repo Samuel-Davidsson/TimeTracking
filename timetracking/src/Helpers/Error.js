8acbd9804fa7f3a975650e8d9b57cc22ada8b201
Samuel <sam.davidsson86@outlook.com>
2019-05-10 23:06:31 +0200

    Removed the old error success files

--- timetracking/src/Helpers/Error.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const error = props => {
-  return (
-    <div>
-      <p className="text-danger">{props.error}</p>
-    </div>
-  );
-};
-export default error;
--- timetracking/src/Helpers/Success.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const success = props => {
-  return (
-    <div>
-      <p className="text-success">{props.success}</p>
-    </div>
-  );
-};
-export default success;
